d9ce072236afc0505e3c058b9f854e1e07a0f79f
Boldeusz <mnpower@poczta.onet.pl>
2020-03-25 15:45:50 +0100

    change

--- core/src/main/java/org/marcin/GameImpl.java
+++ core/src/main/java/org/marcin/GameImpl.java
@@ -2,6 +2,10 @@ package org.marcin;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class GameImpl implements Game {
 
@@ -9,6 +13,7 @@ public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // == fields ==
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -18,14 +23,8 @@ public class GameImpl implements Game {
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
-
-
-    // == public methods ==
-    public void setNumberGenerator(NumberGenerator numberGenerator){
-        this.numberGenerator = numberGenerator;
-    }
-
-
+    // == init ==
+    @PostConstruct
     @Override
     public void reset() {
         smallest = 0;
@@ -36,6 +35,18 @@ public class GameImpl implements Game {
         log.debug("the number is {}",number);
 
     }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info("in Game preDestroy()");
+    }
+
+
+    // == public methods ==
+
+
+
+
 
     @Override
     public int getNumber() {
