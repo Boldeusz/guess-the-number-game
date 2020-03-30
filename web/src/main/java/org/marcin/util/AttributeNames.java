8dab29c02627b0e58f9349eaf7fc396b2955b9f2
Boldeusz <mnpower@poczta.onet.pl>
2020-03-30 13:25:50 +0200

    change

---
+++ web/src/main/java/org/marcin/util/AttributeNames.java
@@ -0,0 +1,13 @@
+package org.marcin.util;
+
+public final class AttributeNames {
+
+    // == constants ==
+    public static final String MAIN_MESSAGE = "mainMessage";
+    public static final String RESULT_MESSAGE = "resultMessage";
+
+    // == Constructor ==
+
+
+    public AttributeNames() {}
+}
---
+++ web/src/main/java/org/marcin/util/ViewNames.java
@@ -0,0 +1,13 @@
+package org.marcin.util;
+
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+
+    // == constructors ==
+
+    public ViewNames() {}
+}
